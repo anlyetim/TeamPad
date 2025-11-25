81f411c95ad75c6af97f6e7bd51630c936a22e72
Anıl Yetim <anil.yetim@ogr.gelisim.edu.tr>
2025-11-25 22:46:10 +0300

    Remove node_modules and .next from repo history

--- .next/dev/static/chunks/app_page_tsx_8b80d226._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_4df93a90._.js",
-  "static/chunks/node_modules_ffd5c46a._.js"
-],
-    source: "dynamic"
-});
